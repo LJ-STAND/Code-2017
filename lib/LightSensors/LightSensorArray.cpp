6bb867164cfb6c0e54f8119f2a8533af46d31de7
jsjyelland <jsjyelland@gmail.com>
2016-10-12 18:54:56 +1000

    LightSensorArray init

--- lib/LightSensors/LightSensorArray.cpp
+++ lib/LightSensors/LightSensorArray.cpp
@@ -1,7 +1,30 @@
 #include "LightSensorArray.h"
 
 LightSensorArray::LightSensorArray() {
-    ls1 = 
+    ls0 = LightSensor(LS_0);
+    ls1 = LightSensor(LS_1);
+    ls2 = LightSensor(LS_2);
+    ls3 = LightSensor(LS_3);
+    ls4 = LightSensor(LS_4);
+    ls5 = LightSensor(LS_5);
+    ls6 = LightSensor(LS_6);
+    ls7 = LightSensor(LS_7);
+    ls8 = LightSensor(LS_8);
+    ls9 = LightSensor(LS_9);
+    ls10 = LightSensor(LS_10);
+    ls11 = LightSensor(LS_11);
+    ls12 = LightSensor(LS_12);
+    ls13 = LightSensor(LS_13);
+    ls14 = LightSensor(LS_14);
+    ls15 = LightSensor(LS_15);
+    ls16 = LightSensor(LS_16);
+    ls17 = LightSensor(LS_17);
+    ls18 = LightSensor(LS_18);
+    ls19 = LightSensor(LS_19);
+    ls20 = LightSensor(LS_20);
+    ls21 = LightSensor(LS_21);
+    ls22 = LightSensor(LS_22);
+    ls23 = LightSensor(LS_23);
 }
 
 void LightSensorArray::read() {

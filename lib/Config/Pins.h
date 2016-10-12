877a7e3de08bd1595a56d927e31b95931e672ac4
jsjyelland <jsjyelland@gmail.com>
2016-10-12 18:47:39 +1000

    Light sensor pins

--- lib/Config/Pins.h
+++ lib/Config/Pins.h
@@ -6,4 +6,30 @@
 #ifndef PINS_H
 #define PINS_H
 
+// Light Sensors
+#define LS_0 A0
+#define LS_1 A1
+#define LS_2 A2
+#define LS_3 A3
+#define LS_4 A4
+#define LS_5 A5
+#define LS_6 A6
+#define LS_7 A7
+#define LS_8 A8
+#define LS_9 A9
+#define LS_10 A10
+#define LS_11 A11
+#define LS_12 A12
+#define LS_13 A13
+#define LS_14 A14
+#define LS_15 A15
+#define LS_16 A16
+#define LS_17 A17
+#define LS_18 A18
+#define LS_19 A19
+#define LS_20 A20
+#define LS_21 A21
+#define LS_22 A22
+#define LS_23 A23
+
 #endif // PINS_H

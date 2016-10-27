f374245b8be46cb7de85bb28ebd23c072f13c66f
jsjyelland <jsjyelland@gmail.com>
2016-10-27 21:20:44 +1000

    More SPI

--- slave_light/src/main.cpp
+++ slave_light/src/main.cpp
@@ -3,13 +3,23 @@
  */
 
 #include <Arduino.h>
+#include <t3spi.h>
 #include <LightSensorArray.h>
 #include <LinePosition.h>
+
+T3SPI spi;
 
 LightSensorArray lightSensorArray;
 LinePosition linePosition;
 
 void setup() {
+    Serial.begin(9600);
+
+    spi.begin_SLAVE(ALT_SCK, MOSI, MISO, CS0);
+    spi.setCTAR_SLAVE(16, SPI_MODE0);
+
+    NVIC_ENABLE_IRQ(IRQ_SPI0);
+
     lightSensorArray.init();
 }
 
